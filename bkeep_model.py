5acdb448f98e648a6718720c5e96c3f64c9fdd15
JernejL123 <154522261+JernejL123@users.noreply.github.com>
2026-01-15 18:55:11 +0100

    Dev naucen model in README

--- bkeep_model.py
+++ bkeep_model.py
@@ -3,9 +3,11 @@ from ultralytics import YOLO
 
 if __name__ == "__main__":
     # Load a model
-    model = YOLO("yolo26n.yaml")  # build a new model from YAML
-    model = YOLO("yolo26n.pt")  # load a pretrained model (recommended for training)
-    model = YOLO("yolo26n.yaml").load("yolo26n.pt")  # build from YAML and transfer weights
+    #model = YOLO("yolo26n.yaml")  # build a new model from YAML
+    model = YOLO("runs/detect/train/weights/last.pt")
 
     # Train the model
-    results = model.train(data="data.yaml", epochs=100, imgsz=640, device=0)
+    #results = model.train(data="data.yaml", epochs=100, resume=True, imgsz=640, device=0)
+
+    #resume training
+    results = model.train(resume=True, device=0, workers=0)

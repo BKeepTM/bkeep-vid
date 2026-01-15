f5baa59328a6a3471422f369961a4c42eeea5d23
JernejL123 <jernej.lobnik@gmail.com>
2026-01-15 10:42:33 +0100

    BRV-1 YOLO8 konfiguracija za ucenje modela

---
+++ bkeep_model.py
@@ -0,0 +1,11 @@
+from ultralytics import YOLO
+
+
+if __name__ == "__main__":
+    # Load a model
+    model = YOLO("yolo26n.yaml")  # build a new model from YAML
+    model = YOLO("yolo26n.pt")  # load a pretrained model (recommended for training)
+    model = YOLO("yolo26n.yaml").load("yolo26n.pt")  # build from YAML and transfer weights
+
+    # Train the model
+    results = model.train(data="data.yaml", epochs=100, imgsz=640, device=0)
